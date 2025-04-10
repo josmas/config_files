ab9aab670f7c8ebb87309186f5572b4d50e3c053
josmas <josmasflores@gmail.com>
2025-04-10 12:45:19 +0100

    macro for console.log in JS/TS files

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+-- Automate console log when selecting a var and running @l
+local esc = vim.api.nvim_replace_termcodes("<ESC>", true, true, true)
+vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "JSLogMacro",
+  pattern = { "javascript", "typescript" },
+  callback = function()
+    vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
+  end,
+})
